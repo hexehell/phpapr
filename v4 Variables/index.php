930c23685dc2eafb371a310e982cac38fdfb8361
hexehell <mr.diaz@live.com>
2018-04-17 17:20:04 -0600

    video 4 del tutorial variables

---
+++ v4 Variables/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>v4</title>
+</head>
+<body>
+
+
+<!-- <form method="POST" action="index.php"> -->
+<!--  -->
+	<!-- User Name: <input type="text" name="user_name" /> -->
+	<!-- Location: <input type="text" name="user_location" /> -->
+	<!-- <input type="submit" value="Send My Info" /> -->
+<!--  -->
+<!-- </form> -->
+
+
+
+<?php
+
+print "<p>Tu mama esta pelona</p>";
+
+
+print "<p>tu mama tambien</p>";
+
+
+$nombre = "jefatu";
+
+
+print "<p>tu mama se llama ". $nombre."</p>";
+
+print "<p>tu mama se llama $nombre</p>";
+
+
+
+?>
+
+	
+</body>
+</html>

79e210753a1ef509dba5f55aa877373c05df683c
hexehell <mr.diaz@live.com>
2018-04-18 10:23:57 -0600

    termine video 10

---
+++ v10 Constantes/index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>v9 Operadores de Comparacion</title>
+
+</head>
+<style>
+	
+	span
+	{
+
+		margin-right:  25px
+	}
+
+		h1{
+		text-align:center;
+	}
+
+	table{
+		background-color:#FFC;
+		padding:5px;
+		border:#666 5px solid;
+	}
+	
+	.no_validado{
+		font-size:18px;
+		color:#F00;
+		font-weight:bold;
+		text-align: center;
+		
+	}
+	
+	.validado{
+		font-size:18px;
+		color:#0C3;
+		font-weight:bold;
+		text-align: center;
+	}
+</style>
+
+
+<body>
+
+
+
+<h1>constantes</h1>
+
+
+
+<?php
+
+define("COLOR","#990000"); // existe un parametro que permite hacer la constante caso insensible
+
+ECHO COLOR;
+
+echo "<p>mira cosa util, numero de linea: ". __LINE__." </p>";
+
+include ("fnFunciones.php");
+
+?>
+
+
+
+	
+</body>
+</html>

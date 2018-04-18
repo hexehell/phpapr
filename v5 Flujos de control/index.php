b9a537fcc0f9c3c4c4752f871131b04a6ff59c10
hexehell <mr.diaz@live.com>
2018-04-17 18:30:41 -0600

    respaldo video 5

---
+++ v5 Flujos de control/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>v5 flujo de control</title>
+</head>
+<body>
+
+
+<!-- <form method="POST" action="index.php"> -->
+<!--  -->
+	<!-- User Name: <input type="text" name="user_name" /> -->
+	<!-- Location: <input type="text" name="user_location" /> -->
+	<!-- <input type="submit" value="Send My Info" /> -->
+<!--  -->
+<!-- </form> -->
+
+
+
+<?php
+
+print "<p>Tu mama esta pelona</p>";
+
+
+print "<p>tu mama tambien</p>";
+
+
+
+?>
+
+	
+</body>
+</html>
